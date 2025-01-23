03b3c10956e7abc5b973806a24ad2a8b72412d26
Ricardo <ricardo.kanashiro.adm@gmail.com>
2025-01-23 17:55:08 -0300

    feat(administrador repository tests): estruturar testes englobando por métodos & adicionar teste de update para email duplicado

--- src/repositories/administrador.repository.test.ts
+++ src/repositories/administrador.repository.test.ts
@@ -30,78 +30,114 @@ describe("Testando métodos do administrador repository", () => {
       `)
    })
 
-   test("Método get() deve retornar lista vazia", async () => {
-      
-      const result = await administradorRepository.get()
+   describe("Método get()", () => {
 
-      expect(result).toBeDefined()
-      expect(result.length).toBe(0)
-      expect(result).toEqual([])
+      test("Deve retornar lista vazia", async () => {
+         
+         const result = await administradorRepository.get()
+   
+         expect(result).toBeDefined()
+         expect(result.length).toBe(0)
+         expect(result).toEqual([])
+      })
    })
 
-   test("Método create() deve retornar administrador cadastrado", async () => {
 
-      const fakeAdministrador = {
-         id: "1",
-         nome: "Ricardo Kanashiro",
-         email: "ricardo@email.com",
-         senha: "senha123"
-      }
+   describe("Método create()", () => {
 
-      const result = await administradorRepository.create(fakeAdministrador)
+      test("Deve retornar administrador cadastrado", async () => {
 
-      expect(result).toBeDefined()
-      expect(result.length).toBe(1)
-      expect(result).toEqual([ fakeAdministrador ])
+         const fakeAdministrador = {
+            id: "1",
+            nome: "Ricardo Kanashiro",
+            email: "ricardo@email.com",
+            senha: "senha123"
+         }
+   
+         const result = await administradorRepository.create(fakeAdministrador)
+   
+         expect(result).toBeDefined()
+         expect(result.length).toBe(1)
+         expect(result).toEqual([ fakeAdministrador ])
+      })
+   
+      test("Não pode criar administradores com ids duplicados", async () => {
+   
+         const sameIdFakeAdministrador = {
+            id: "1",
+            nome: "Outro Ricardo",
+            email: "outroricardo@email.com",
+            senha: "senha123"
+         }
+   
+         await expect(administradorRepository.create(sameIdFakeAdministrador)).rejects.toThrow()
+      })
+   
+      test("Não pode criar administradores com emails duplicados", async () => {
+   
+         const sameIdFakeAdministrador = {
+            id: "2",
+            nome: "Outro Ricardo",
+            email: "ricardo@email.com",
+            senha: "senha123"
+         }
+   
+         await expect(administradorRepository.create(sameIdFakeAdministrador)).rejects.toThrow()
+      })
    })
 
-   test("Método get() não pode criar administradores com ids duplicados", async () => {
+   describe("Método update()", () => {
 
-      const sameIdFakeAdministrador = {
-         id: "1",
-         nome: "Outro Ricardo",
-         email: "outroricardo@email.com",
-         senha: "senha123"
-      }
+      test("Deve atualizar e retornar administrador", async () => {
 
-      await expect(administradorRepository.create(sameIdFakeAdministrador)).rejects.toThrow()
+         const fakeData = {
+            nome: "Ricardo Atualizado",
+            id: "1"
+         }
+   
+         const result = await administradorRepository.update(fakeData)
+   
+         expect(result).toBeDefined()
+         expect(result.length).toBe(1)
+         expect(result).toEqual([{ ...fakeData, email: "ricardo@email.com", senha: "senha123" }])
+      })
+   
+      test("Não deve atualizar caso Email esteja duplicado", async () => {
+   
+         const fakeAnotherAdm = {
+            id: "2",
+            nome: "Outro Ricardo",
+            email: "ricardooutro@email.com",
+            senha: "senha123"
+         }
+   
+         await administradorRepository.create(fakeAnotherAdm)
+   
+         const fakeData = {
+            email: "ricardo@email.com",
+            id: "2"
+         }
+   
+         await expect(administradorRepository.update(fakeData)).rejects.toThrow()
+      })
    })
 
-   test("Método get() não pode criar administradores com emails duplicados", async () => {
+   describe("Método delete()", () => {
 
-      const sameIdFakeAdministrador = {
-         id: "2",
-         nome: "Outro Ricardo",
-         email: "ricardo@email.com",
-         senha: "senha123"
-      }
-
-      await expect(administradorRepository.create(sameIdFakeAdministrador)).rejects.toThrow()
+      test("Deve deletar administradores existentes", async () => {
+   
+         const fakeIds = ["1", "2"]
+   
+         for(let id of fakeIds) {
+            await administradorRepository.delete(id)
+         }
+   
+         const result = await administradorRepository.get()
+   
+         expect(result).toBeDefined()
+         expect(result.length).toBe(0)
+         expect(result).toEqual([])
+      })
    })
 
-   test("Método update() deve atualizar e retornar administrador", async () => {
-
-      const fakeData = {
-         nome: "Ricardo Atualizado",
-         id: "1"
-      }
-
-      const result = await administradorRepository.update(fakeData)
-
-      expect(result).toBeDefined()
-      expect(result.length).toBe(1)
-      expect(result).toEqual([{ ...fakeData, email: "ricardo@email.com", senha: "senha123" }])
-   })
-
-   test("Método delete() deve deletar administrador", async () => {
-
-      const fakeId = "1"
-      await administradorRepository.delete(fakeId)
-
-      const result = await administradorRepository.get()
-
-      expect(result).toBeDefined()
-      expect(result.length).toBe(0)
-      expect(result).toEqual([])
-   })
 })
